92e8734a9822add17f3f48da584ff0bffaed4bad
opmarketgarden <murugu1957murugu1957@gmail.com>
2021-10-16 14:44:10 +0530

    added sufireplugin and moved test to src/test/java and renamed to Test.java

--- src/test/java/Google_Test/NewTest.java
+++ src/test/java/Google_Test/NewTest.java
@@ -1,15 +1,28 @@
 package Google_Test;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+
+import java.sql.Driver;
 import java.util.PriorityQueue;
 
-import org.testng.annotations.Test;
 
 public class NewTest {
 	
   @Test(groups = {"smoke"})
   public void googlelaunch() {
 	  System.out.println("hi");
-	  
+  }
+  
+  @Test(groups = {"smoke"})
+  public void launch() {
+	  System.out.println("hi1");
+  }
+  
+  @Test(groups = {"smoke"})
+  public void launch2() {
+	 WebDriver driver = new ChromeDriver();
+	 driver.get("https://google.com");
   }
 }
---
+++ src/test/java/Google_Test/otherTest.java
@@ -0,0 +1,12 @@
+package Google_Test;
+
+import org.testng.annotations.Test;
+
+public class otherTest {
+	
+	@Test(groups = {"smoke"})
+	public void CheckTest() {
+		System.out.println("passed");
+	}
+
+}
